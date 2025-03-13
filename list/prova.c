c1dcab06a3a79a266a0759caf114517a59737d46
LucaForamacchi <162980736+LucaForamacchi@users.noreply.github.com>
2025-03-13 21:31:03 +0100

    Update prova.c

--- list/prova.c
+++ list/prova.c
@@ -14,78 +14,22 @@ int main(int argc, char* argv[]) {
     else
         printf(" La lista non e' vuota \n");
 
-    printf("Fase di Inserimento dati\n");
-
-    printf("La lista contiene %d elementi\n", getLength(mylist));
-
+    i.value = 1;
+    mylist = EnqueueLast(mylist,i);
+    
+    i.value = 2;
+    mylist = EnqueueLast(mylist,i);
+    
+    i.value = 3;
+    mylist = EnqueueLast(mylist,i);
+    
+    i.value = 4;
+    mylist = EnqueueLast(mylist,i);
+    
     printf("Contenuto della lista: [");
     PrintList(mylist);
     printf("]\n");
-
-    i.value = 4.004;
-    mylist = EnqueueLast(mylist, i);
-
-    printf("La lista contiene %d elementi\n", getLength(mylist));
-
-    i.value = 3.34;
-    mylist = EnqueueLast(mylist, i);
-
-    printf("La lista contiene %d elementi \n", getLength(mylist));
-
-    i.value = 0.42;
-    mylist = EnqueueLast(mylist, i);
-
-    printf("La lista contiene %d elementi\n", getLength(mylist));
-
-    i.value = 1.2345;
-    mylist = EnqueueLast(mylist, i);
-
-    printf("La lista contiene %d elementi\n", getLength(mylist));
-
-    i.value = 1.1823;
-    mylist = EnqueueLast(mylist, i);
-
-    printf("La lista contiene %d elementi\n", getLength(mylist));
-
-    printf("Contenuto della lista: [");
-    PrintList(mylist);
-    printf("]\n");
-
-    printf("Elemento di testa ");
-    PrintItem(getHead(mylist));
-    printf("\n");
-
-    printf("Elemento di coda ");
-    PrintItem(getTail(mylist));
-    printf("\n");
-
-    printf("Fase di Estrazione dati\n");
-
-    i.value = 1.2345;
-    printf("Estrazione elemento ");
-    PrintItem(i);
-    printf("\n");
-    mylist = Dequeue(mylist, i);
-
-    i.value = 9.9999;
-    printf("Estrazione elemento ");
-    PrintItem(i);
-    printf("\n");
-    mylist = Dequeue(mylist, i);
-
-    i.value = 4.004;
-    printf("Estrazione elemento ");
-    PrintItem(i);
-    printf("\n");
-    mylist = Dequeue(mylist, i);
-
-    printf("Estrazione primo elemento\n");
-    mylist = DequeueFirst(mylist);
-
-    printf("Contenuto della lista: [");
-    PrintList(mylist);
-    printf("]\n");
-
+    
     i.value = 5;
     mylist = EnqueueOrdered(mylist,i, 3);
 
